2a6a6d08f0d4d2333f389771c332df7ceb13b81a
Dmitry Baranov <mainarchi@yandex.ru>
2025-09-30 23:57:26 +0300

    -Добавил логгирование в каждом классе. -Настроил конфигурацию log4j. Все, что info и выше пишется в файл main, а все что дебаг и выше пишется в degug

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 import layer.ClientLevel;
 import layer.DataLevel;
-import layer.dto.Message;
 import layer.PackagingLevel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utlis.ClientConfig;
-
-import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
