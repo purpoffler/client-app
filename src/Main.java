b6bb86ac8cb2a968bba35df20d397cf2635e257f
Dmitry Baranov <mainarchi@yandex.ru>
2025-10-12 00:37:24 +0300

    -лишний final в ClientConfig

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ import layer.loggers.Slf4jAdapter;
 import utlis.ClientConfig;
 
 public class Main {
-    //private  static AppLogger log = new Slf4jAdapter(Main.class);
-    private static AppLogger log = new CustomLogger(Main.class);
-
     public static void main(String[] args) throws Exception {
         ClientConfig.init();
+
+        //AppLogger log = new Slf4jAdapter(Main.class);
+        AppLogger log = new CustomLogger(Main.class);
 
         log.info("Программа запустилась");
 
